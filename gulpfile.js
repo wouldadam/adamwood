aa16cf9591619171e8f857bac84639008ff68ca0
Adam Wood <adam@adamwood.org>
2019-11-26 00:53:35 +0000

    Update dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -14,66 +14,69 @@ var jekyllCommand = (/^win/.test(process.platform)) ? 'jekyll.bat' : 'jekyll';
  * Build the Jekyll Site
  * runs a child process in node that runs the jekyll commands
  */
-gulp.task('jekyll-build', function (done) {
-	return cp.spawn(jekyllCommand, ['build'], {stdio: 'inherit'})
+function jekyllBuild(done) {
+	return cp.spawn(jekyllCommand, ['build'], { stdio: 'inherit' })
 		.on('close', done);
-});
+};
 
 /*
  * Rebuild Jekyll & reload browserSync
  */
-gulp.task('jekyll-rebuild', ['jekyll-build'], function () {
+function jekyllRebuild() {
 	browserSync.reload();
-});
+};
 
 /*
  * Build the jekyll site and launch browser-sync
  */
-gulp.task('browser-sync', ['jekyll-build'], function() {
+function browserSync() {
 	browserSync({
 		server: {
 			baseDir: '_site'
-		}
-	});
-});
+		},
 
-/*
-* Compile and minify sass
-*/
-gulp.task('sass', function() {
-  gulp.src('src/styles/**/*.scss')
-    .pipe(plumber())
-    .pipe(sass())
-    .pipe(csso())
-    .pipe(gulp.dest('assets/css'));
-});
+	});
+};
+
+// /*
+// * Compile and minify sass
+// */
+function css() {
+	return gulp.src('src/styles/**/*.scss')
+		.pipe(plumber())
+		.pipe(sass())
+		.pipe(csso())
+		.pipe(gulp.dest('assets/css'));
+};
 
 /*
  * Minify images
  */
-gulp.task('imagemin', function() {
+function imageMin() {
 	return gulp.src('src/img/**/*.{jpg,png,gif}')
 		.pipe(plumber())
 		.pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
 		.pipe(gulp.dest('assets/img/'));
-});
+};
 
 /**
  * Compile and minify js
  */
-gulp.task('js', function(){
+function js() {
 	return gulp.src('src/js/**/*.js')
 		.pipe(plumber())
 		.pipe(concat('main.js'))
 		.pipe(uglify())
 		.pipe(gulp.dest('assets/js/'))
-});
+};
 
-gulp.task('watch', function() {
-  gulp.watch('src/styles/**/*.scss', ['sass']);
-  gulp.watch('src/js/**/*.js', ['js']);
-	gulp.watch('src/img/**/*.{jpg,png,gif}', ['imagemin']);
-  gulp.watch(['*html', '_includes/*html', '_layouts/*.html'], ['jekyll-rebuild']);
-});
+function watch() {
+	gulp.watch('src/styles/**/*.scss', css);
+	gulp.watch('src/js/**/*.js', js);
+	gulp.watch('src/img/**/*.{jpg,png,gif}', imageMin);
+	gulp.watch(['*html', '_includes/*html', '_layouts/*.html'], jekyllRebuild);
+};
 
-gulp.task('default', ['js', 'sass', 'browser-sync', 'watch']);
+exports.build = gulp.series(js, css, jekyllBuild)
+exports.watch = gulp.series(exports.build, browserSync, watch)
+exports.default = exports.watch
